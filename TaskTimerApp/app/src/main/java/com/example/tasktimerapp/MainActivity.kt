7ef798e94ae2027e60b8e6c0d7670836eac9258a
CompanionObject <ghadeeralshehri@outlook.com>
2022-01-05 14:28:13 +0300

    #Create View Model & Repository...

--- TaskTimerApp/app/src/main/java/com/example/tasktimerapp/MainActivity.kt
+++ TaskTimerApp/app/src/main/java/com/example/tasktimerapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.tasktimerapp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tasktimerapp.ViewModel.TaskViewModel
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val taskViewModel by lazy { TaskViewModel(application) }
+    private lateinit var sharedPreferences: SharedPreferences
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sharedPreferences = this.getSharedPreferences(
+            getString(R.string.preference_file_key), MODE_PRIVATE
+        )
+
+
     }
+
+
 }
