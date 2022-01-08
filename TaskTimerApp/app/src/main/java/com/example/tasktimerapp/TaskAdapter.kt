e01999245dd5a748b1976e89f893716eae9999f2
Ghaidaa Hussain <gadhus@outlook.com>
2022-01-08 14:43:39 +0300

    fix Add task - read tasks xml

--- TaskTimerApp/app/src/main/java/com/example/tasktimerapp/TaskAdapter.kt
+++ TaskTimerApp/app/src/main/java/com/example/tasktimerapp/TaskAdapter.kt
@@ -21,7 +21,8 @@ class TaskAdapter(var list: List<Task>, private val activity: Activity): Recycle
         holder.binding.apply {
             tvTitle.text = item.task
             tvDes.text = item.description
-            tvTimer.text = "0:${item.time}"
+            tvTimer.text = "0:${(item.time.toDouble() / 1000.0).toInt()}"
+            timeProgress.progress = ((item.time.toDouble() / 60000.0) * 100).toInt()
         }
 
         holder.binding.taskRow.setOnClickListener {
