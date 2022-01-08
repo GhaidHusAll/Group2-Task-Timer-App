8f1312ccaa24e88cf5900fde6dd69d46598511e0
Ghaidaa Hussain <gadhus@outlook.com>
2022-01-08 19:24:46 +0300

    commit to pull

---
+++ TaskTimerApp/app/build/tmp/kapt3/stubs/debug/com/example/tasktimerapp/database/DatabaseApp.java
@@ -0,0 +1,33 @@
+package com.example.tasktimerapp.database;
+
+import java.lang.System;
+
+@androidx.room.Database(entities = {com.example.tasktimerapp.database.Task.class}, version = 2, exportSchema = false)
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\'\u0018\u0000 \u00052\u00020\u0001:\u0001\u0005B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H&\u00a8\u0006\u0006"}, d2 = {"Lcom/example/tasktimerapp/database/DatabaseApp;", "Landroidx/room/RoomDatabase;", "()V", "dao", "Lcom/example/tasktimerapp/database/TaskDao;", "Companion", "app_debug"})
+public abstract class DatabaseApp extends androidx.room.RoomDatabase {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.example.tasktimerapp.database.DatabaseApp.Companion Companion = null;
+    @kotlin.jvm.Volatile()
+    private static volatile com.example.tasktimerapp.database.DatabaseApp INSTANCE;
+    
+    public DatabaseApp() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public abstract com.example.tasktimerapp.database.TaskDao dao();
+    
+    @kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0005\u001a\u00020\u00042\u0006\u0010\u0006\u001a\u00020\u0007R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\b"}, d2 = {"Lcom/example/tasktimerapp/database/DatabaseApp$Companion;", "", "()V", "INSTANCE", "Lcom/example/tasktimerapp/database/DatabaseApp;", "getDatabase", "context", "Landroid/content/Context;", "app_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final com.example.tasktimerapp.database.DatabaseApp getDatabase(@org.jetbrains.annotations.NotNull()
+        android.content.Context context) {
+            return null;
+        }
+    }
+}
---
+++ TaskTimerApp/app/build/tmp/kapt3/stubs/debug/com/example/tasktimerapp/database/Task.java
@@ -0,0 +1,107 @@
+package com.example.tasktimerapp.database;
+
+import java.lang.System;
+
+@androidx.room.Entity()
+@kotlin.Metadata(mv = {1, 5, 1}, k = 1, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\t\n\u0000\n\u0002\u0010\u000b\n\u0002\b\u001d\b\u0087\b\u0018\u00002\u00020\u0001B-\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0005\u0012\u0006\u0010\u0007\u001a\u00020\b\u0012\u0006\u0010\t\u001a\u00020\n\u00a2\u0006\u0002\u0010\u000bJ\t\u0010\u001d\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001e\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0005H\u00c6\u0003J\t\u0010 \u001a\u00020\bH\u00c6\u0003J\t\u0010!\u001a\u00020\nH\u00c6\u0003J;\u0010\"\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00052\b\b\u0002\u0010\u0007\u001a\u00020\b2\b\b\u0002\u0010\t\u001a\u00020\nH\u00c6\u0001J\u0013\u0010#\u001a\u00020\n2\b\u0010$\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010%\u001a\u00020\u0003H\u00d6\u0001J\t\u0010&\u001a\u00020\u0005H\u00d6\u0001R\u001a\u0010\u0006\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\f\u0010\r\"\u0004\b\u000e\u0010\u000fR\u001a\u0010\t\u001a\u00020\nX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\t\u0010\u0010\"\u0004\b\u0011\u0010\u0012R\u001e\u0010\u0002\u001a\u00020\u00038\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0013\u0010\u0014\"\u0004\b\u0015\u0010\u0016R\u001a\u0010\u0004\u001a\u00020\u0005X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0017\u0010\r\"\u0004\b\u0018\u0010\u000fR\u001a\u0010\u0007\u001a\u00020\bX\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0019\u0010\u001a\"\u0004\b\u001b\u0010\u001c\u00a8\u0006\'"}, d2 = {"Lcom/example/tasktimerapp/database/Task;", "", "pk", "", "task", "", "description", "time", "", "isDone", "", "(ILjava/lang/String;Ljava/lang/String;JZ)V", "getDescription", "()Ljava/lang/String;", "setDescription", "(Ljava/lang/String;)V", "()Z", "setDone", "(Z)V", "getPk", "()I", "setPk", "(I)V", "getTask", "setTask", "getTime", "()J", "setTime", "(J)V", "component1", "component2", "component3", "component4", "component5", "copy", "equals", "other", "hashCode", "toString", "app_debug"})
+public final class Task {
+    @androidx.room.PrimaryKey(autoGenerate = true)
+    private int pk;
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String task;
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String description;
+    private long time;
+    private boolean isDone;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.example.tasktimerapp.database.Task copy(int pk, @org.jetbrains.annotations.NotNull()
+    java.lang.String task, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, long time, boolean isDone) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public Task(int pk, @org.jetbrains.annotations.NotNull()
+    java.lang.String task, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, long time, boolean isDone) {
+        super();
+    }
+    
+    public final int component1() {
+        return 0;
+    }
+    
+    public final int getPk() {
+        return 0;
+    }
+    
+    public final void setPk(int p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTask() {
+        return null;
+    }
+    
+    public final void setTask(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getDescription() {
+        return null;
+    }
+    
+    public final void setDescription(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    public final long component4() {
+        return 0L;
+    }
+    
+    public final long getTime() {
+        return 0L;
+    }
+    
+    public final void setTime(long p0) {
+    }
+    
+    public final boolean component5() {
+        return false;
+    }
+    
+    public final boolean isDone() {
+        return false;
+    }
+    
+    public final void setDone(boolean p0) {
+    }
+}
